294fda109670a98385ceb3704572af591ecc2f5a
readykevin <gifte.yang@gmail.com>
2020-02-22 10:53:03 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "KY的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+author = "KY"
+email = "gifte.yang@gmail.com"
+author_homepage = ""
+description = "desc sample"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
